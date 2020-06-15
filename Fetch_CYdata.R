399bb47074e4d340614f6c98e896a6245a1f6f84
SolomonI <sioann88@gmail.com>
2020-06-15 18:38:24 +0200

    Evaluating commit

--- Fetch_CYdata.R
+++ Fetch_CYdata.R
@@ -1,2 +1,2 @@
-URL = "https://www.data.gov.cy/sites/default/files/CY%20Covid19%20Daily%20Statistics_15.csv"
+URL = "https://www.data.gov.cy/sites/default/files/CY%20Covid19%20Daily%20Statistics_21.csv"
 dfcy <- read.csv(URL, header = TRUE)
